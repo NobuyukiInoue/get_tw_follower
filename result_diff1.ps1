10a158a5a368627d137698002dcdd2b0b3c44d45
Nobuyuki Inoue <gx3n-inue@asahi-net.or.jp>
2018-10-27 11:45:55 +0900

    modify README.md

--- result_diff1.ps1
+++ result_diff1.ps1
@@ -1,13 +1,13 @@
 Param( $file1, $file2 )
 
 if (-Not($file1)) {
-	Write-Host "Usage : result_diff.ps1 file1 file2"
-	exit 0
+    Write-Host "Usage :"$MyInvocation.MyCommand.Name "file1 file2"
+    exit 0
 }
 
 if (-Not($file2)) {
-	Write-Host "Usage : result_diff.ps1 file1 file2"
-	exit 0
+    Write-Host "Usage :"$MyInvocation.MyCommand.Name "file1 file2"
+    exit 0
 }
 
 $A = Get-Content $file1
